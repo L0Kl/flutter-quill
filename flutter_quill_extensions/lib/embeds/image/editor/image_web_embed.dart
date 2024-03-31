0618c5626190a6bab3a8ca3f410ac8a9666cbe3e
L0Kl <70410565+L0Kl@users.noreply.github.com>
2024-03-31 21:41:01 +0200

    base64Decode width and height II

--- flutter_quill_extensions/lib/embeds/image/editor/image_web_embed.dart
+++ flutter_quill_extensions/lib/embeds/image/editor/image_web_embed.dart
@@ -52,13 +52,14 @@ class QuillEditorWebImageEmbedBuilder extends EmbedBuilder {
       }
     }
 
-    // var imageBytes = base64Decode(imageSource);
-    // img.Image? image = img.decodeImage(imageBytes);
-    // if (image != null) {
-    //   imageWidth = image.width as double;
-    //   imageHeight = image.height as double;
-    //   print('Breite: ${image.width}, Höhe: ${image.height}');
-    // }
+    var base64Content = imageSource.split(',')[1];
+    var imageBytes = base64Decode(base64Content);
+    img.Image? image = img.decodeImage(imageBytes);
+    if (image != null) {
+      imageWidth = image.width as double;
+      imageHeight = image.height as double;
+      print('Breite: ${image.width}, Höhe: ${image.height}');
+    }
 
     ui.PlatformViewRegistry().registerViewFactory(imageSource, (viewId) {
       return html.ImageElement()
